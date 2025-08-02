a111244275d6fcc8a96f6ddc32e71fcb8ecf07be
Abhilash G S <abhiabhigs2005@gmail.com>
2025-08-02 12:11:38 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println(3,3);
+        path("",3,3);
+    }
+    static int count(int r,int c){
+        if(r==1||c==1){
+            return 1;
+        }
+        int left=count(r-1,c);
+        int right=count(r,c-1);
+        return left+right;
+    }
+    static void path(String p,int r,int c ){
+        if (r==1&&c==1){
+            System.out.println(p);
+            return;
+        }
+        if(r>1){
+            path(p+'D',r-1,c);
+        }
+        if(c>1){
+            path(p+'R',r,c-1);
+        }
+    }
+}
